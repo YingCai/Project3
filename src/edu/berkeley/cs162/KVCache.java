d7741ba8710fd82166afe21c286ddbb769e48a91
Lewis Ren <lewisren@gmail.com>
2013-04-17 23:47:22 -0700

    added rag's shit + tests

--- src/edu/berkeley/cs162/KVCache.java
+++ src/edu/berkeley/cs162/KVCache.java
@@ -52,15 +52,15 @@ public class KVCache implements KeyValueInterface {
 	private HashMap<String,Boolean> usedbits = new HashMap<String,Boolean>();
 	private HashMap<String,String> contents = new HashMap<String,String>();
 	private HashMap<String,Boolean> validbits = new HashMap<String,Boolean>();
-	private ReentrantReadWriteLock[] setlocks;
+	private ReentrantReadWriteLock[] setlocks = new ReentrantReadWriteLock[numSets];
 
 	/**
 	 * Creates a new LRU cache.
 	 * @param cacheSize	the maximum number of entries that will be kept in this cache.
 	 */
 	public KVCache(int numSets, int maxElemsPerSet) {
-		numSets = numSets;
-		maxElemsPerSet = maxElemsPerSet;
+		this.numSets = numSets;
+		this.maxElemsPerSet = maxElemsPerSet;
 		sets = (LinkedList<String>[]) new LinkedList[numSets];
 		// TODO: Implement Me!
 		for (int i = 0 ; i < numSets ; i++){
@@ -111,10 +111,6 @@ public class KVCache implements KeyValueInterface {
 		AutoGrader.agCachePutStarted(key, value);
 		AutoGrader.agCachePutDelay();
 
-		if (key.length() > maxKey || value.length() > maxVal ){
-			System.out.println("The key or Value is too big!!!");
-		}
-
 		int setId = getSetId(key);
 		LinkedList set = sets[setId];
 
@@ -129,21 +125,30 @@ public class KVCache implements KeyValueInterface {
 				boolean finished = false;
 				for (int i = 0 ; i < maxElemsPerSet ; i++){
 					String oldkey = (String)set.get(i);
-					if(!usedbits.get(oldkey)){
-						finished = true;
-						set.remove(oldkey);
-						set.add(key);
-						contents.remove(oldkey);
-						contents.put(key, value);
-						break;
+					try{
+						if(!usedbits.get(oldkey)){
+							finished = true;
+							set.remove(oldkey);
+							set.add(key);
+							usedbits.put(oldkey,false);
+							contents.remove(oldkey);
+							usedbits.put(key, false);
+							contents.put(key, value);
+							break;
+						}
+					}
+					catch(Exception e){
+						System.out.println(e + " , " + oldkey + " , " + usedbits.get(oldkey));
+						System.exit(0);
 					}
 				}
 				if (!finished){
 					String oldkey = (String)set.get(0);
-					usedbits.put(oldkey,false);
 					set.remove(oldkey);
 					set.add(key);
+					usedbits.put(oldkey,false);
 					contents.remove(oldkey);
+					usedbits.put(key, false);
 					contents.put(key, value);
 				}
 			}
