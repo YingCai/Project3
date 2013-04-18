5219e14fe820cba27445f764fd68e23e05bd1b05
Jimmy Zhang <jzhang621@gmail.com>
2013-04-18 03:26:03 -0700

    reverted shell script back to premerge version

--- src/proj3.sh
+++ src/proj3.sh
@@ -2,10 +2,15 @@
 
 cd /Users/jzhang621/Documents/Berkeley/CS162/projects/Project3/src/edu/berkeley/cs162/
 
-javac *.java 2> /dev/null
+javac *.java 
 
 if [ $? -eq 0 ]
 then  
-cd /Users/jzhang621/Documents/Berkeley/CS162/projects/Project3/src
-java edu.berkeley.cs162.$1
+    cd /Users/jzhang621/Documents/Berkeley/CS162/projects/Project3/src
+    if test $1 = "KVCache" ; then 
+        echo "Results will be written to results.xml"
+        java edu.berkeley.cs162.$1 > results.xml
+    else 
+        java edu.berkeley.cs162.$1
+    fi
 fi
